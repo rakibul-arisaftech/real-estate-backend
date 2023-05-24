e36e9a9e989aca87a71684b79ef48ab00c1c3a3e
rakibul <rakibul.arisaftech@gmail.com>
2023-05-24 11:48:54 +0600

    added config for deployment

--- backend/settings.py
+++ backend/settings.py
@@ -36,9 +36,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -104,16 +104,29 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env('DB_NAME'),
+#         'USER': env('DB_USER'),
+#         'PASSWORD': env('DB_USER_PASSWORD'),
+#         'HOST': env('DB_HOST'),
+#         'PORT': env('DB_DB_PORT'),
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_USER_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_DB_PORT'),
+        'NAME': "realestate",
+        'USER': "postgres",
+        'PASSWORD': "AST@#5212",
+        'HOST': "localhost",
+        'PORT': "5432",
     }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
