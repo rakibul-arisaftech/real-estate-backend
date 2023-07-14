c45c4dedd73c10a06632c4bf8e1c9b47ecdeccf6
AlviAst <127176465+AlviAst@users.noreply.github.com>
2023-07-14 09:16:33 +0600

    Revert "solve the multiple image problem"

--- users/views.py
+++ users/views.py
@@ -216,32 +216,21 @@ class UserLoginAPIView(GenericAPIView):
                 }, status=status.HTTP_401_UNAUTHORIZED)
 
 
-# class UserLogoutAPIView(GenericAPIView):
+class UserLogoutAPIView(GenericAPIView):
     """
     An endpoint to logout users.
     """
 
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
-    # def post(self, request, *args, **kwargs):
-    #     try:
-    #         refresh_token = request.data["refresh"]
-    #         token = RefreshToken(refresh_token)
-    #         token.blacklist()
-    #         return Response(status=status.HTTP_205_RESET_CONTENT)
-    #     except Exception as e:
-    #         return Response(status=status.HTTP_400_BAD_REQUEST)
-
-class LogOutAPIView(APIView):
-    def post(self,request,format=None):
+    def post(self, request, *args, **kwargs):
         try:
-            refresh_token = request.data.get("refresh_token")
-            token_obj = RefreshToken(refresh_token)
-            token_obj.blacklist()
-            return Response(status = status.HTTP_200_OK)
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
-            return Response(status = status.HEEP_400BAD_REQUEST)
-
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserAPIView(RetrieveUpdateAPIView):
